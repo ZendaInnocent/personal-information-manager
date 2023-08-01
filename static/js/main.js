36ea314c85675d92905953a9abf4af498efa2ac4
Innocent Zenda <zendainnocent@gmail.com>
2023-08-01 23:38:48 +0300

    fix messages and toasts

--- static/js/main.js
+++ static/js/main.js
@@ -4,39 +4,27 @@ document.body.addEventListener('htmx:afterSwap', function (evt) {
     form.reset();
 });
 
+htmx.onLoad(() => {
+    htmx.findAll('.toast').forEach((element) => {
+        let toast = bootstrap.Toast.getInstance(element)
 
-htmx.on('messages', function (evt) {
-    evt.detail.value.forEach(createToast);
+        if (!toast) {
+            const toast = new bootstrap.Toast(element)
+            toast.show()
+        } else if (!toast.isShown()) {
+            toast.dispose()
+            element.remove()
+        }
+    })
 })
 
-function createToast(message) {
-    // Clone the template
-    const element = htmx.find("[data-toast-template]").cloneNode(true)
-
-    // Remove the data-toast-template attribute
-    delete element.dataset.toastTemplate
-
-    // Set the CSS class
-    element.className += " " + message.tags
-
-    // Set the text
-    htmx.find(element, "[data-toast-body]").innerText = message.message
-
-    // Add the new element to the container
-    htmx.find("[data-toast-container]").appendChild(element)
-
-    // Show the toast using Bootstrap's API
-    const toast = new bootstrap.Toast(element, { delay: 2000 })
-    toast.show()
-}
-
-htmx.onLoad(function(content) {
+htmx.onLoad(function (content) {
     var sortables = content.querySelectorAll(".sortable");
     for (var i = 0; i < sortables.length; i++) {
-      var sortable = sortables[i];
-      new Sortable(sortable, {
-          animation: 150,
-          ghostClass: 'blue-background-class'
-      });
+        var sortable = sortables[i];
+        new Sortable(sortable, {
+            animation: 150,
+            ghostClass: 'blue-background-class'
+        });
     }
 })
