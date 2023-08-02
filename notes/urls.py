b5e1b90485f61183cd6b9e934b83923a97d2c418
Innocent Zenda <zendainnocent@gmail.com>
2023-08-02 13:17:57 +0300

    add create note functionality

--- notes/urls.py
+++ notes/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from . import views
+
 app_name = 'notes'
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.index, name='notes-index'),
+    path('create/', views.NoteCreateView.as_view(), name='note-create'),
+]
--- notes/views.py
+++ notes/views.py
@@ -1,1 +1,19 @@
+from typing import Any
+
+from django.db import models
 from django.shortcuts import render
+from django.views.generic import CreateView, DetailView, ListView
+
+from . import forms
+from .models import Note
+
+
+def index(request):
+    notes = Note.objects.all()
+    context = {'notes': notes}
+    return render(request, 'notes/index.html', context)
+
+
+class NoteCreateView(CreateView):
+    model = Note
+    form_class = forms.NoteForm
