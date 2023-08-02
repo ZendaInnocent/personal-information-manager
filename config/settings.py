bf298135421881229dc20c7e269b019b20e0aaf2
Innocent Zenda <zendainnocent@gmail.com>
2023-08-02 23:31:27 +0300

    add app for billing

--- config/settings.py
+++ config/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'billing.apps.BillingConfig',
     'notes.apps.NotesConfig',
     'accounts.apps.AccountsConfig',
     'django.contrib.admin',
--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('billing/', include('billing.urls')),
     path('todos/', include('todos.urls')),
     path('notes/', include('notes.urls')),
     path('accounts/', include('accounts.urls')),
