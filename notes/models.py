a1edaf4deeaaddb02fb776d85d1849330eba32c1
Innocent Zenda <zendainnocent@gmail.com>
2023-08-02 13:16:38 +0300

    add Note model

--- notes/models.py
+++ notes/models.py
@@ -1,1 +1,19 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
+
+User = get_user_model()
+
+
+class Note(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='notes',
+    )
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    color = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.title
