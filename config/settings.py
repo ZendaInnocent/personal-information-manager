57d0816680e9342a85510b23a14165cf6bd5a1d3
Innocent Zenda <zendainnocent@gmail.com>
2023-08-02 12:24:33 +0300

    add notes app

--- config/settings.py
+++ config/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'notes.apps.NotesConfig',
     'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todos/', include('todos.urls')),
+    path('notes/', include('notes.urls')),
     path('accounts/', include('accounts.urls')),
     path('__debug__', include('debug_toolbar.urls')),
 ]
