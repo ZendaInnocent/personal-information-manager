4e65f8216bc34ecc901f672bb863d520aff2a345
Innocent Zenda <zendainnocent@gmail.com>
2023-08-16 08:04:16 +0300

    add main app

---
+++ main/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+
+from billing.models.plans import Plan
+
+
+class HomeView(ListView):
+    template_name = 'main/home.html'
+    context_object_name = 'plans'
+
+    def get_queryset(self):
+        return Plan.objects.all()
+
+
+def get_started(request):
+    template_name = 'main/get_started.html'
+    context = {}
+    return render(request, template_name, context)
