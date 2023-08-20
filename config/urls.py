e0645861cb358557e9502097e367c80bd9136b21
Innocent Zenda <zendainnocent@gmail.com>
2023-08-20 16:47:54 +0300

    add language prefix to urls

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,8 @@
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('', include('pim.main.urls')),
     path('admin/', admin.site.urls),
     path('billing/', include('pim.billing.urls')),
@@ -12,4 +13,5 @@ urlpatterns = [
     path('__debug__', include('debug_toolbar.urls')),
     path('tinymce/', include('tinymce.urls')),
     path('rosetta/', include('rosetta.urls')),
-]
+    prefix_default_language=False,
+)
