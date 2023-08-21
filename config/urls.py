437d56a0636792a3bb52fd59f6c720bfc392ae0f
Innocent Zenda <zendainnocent@gmail.com>
2023-08-22 00:00:41 +0300

    fix: change language functionality

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,16 @@ from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = i18n_patterns(
+from pim.main import views as main_views
+
+urlpatterns = [
+    path('set-language/', main_views.set_language, name='set-language'),
+    path('__debug__', include('debug_toolbar.urls')),
+    path('tinymce/', include('tinymce.urls')),
+    path('rosetta/', include('rosetta.urls')),
+]
+
+urlpatterns += i18n_patterns(
     path('', include('pim.main.urls')),
     path('admin/', admin.site.urls),
     path('billing/', include('pim.billing.urls')),
@@ -10,8 +19,5 @@ urlpatterns = i18n_patterns(
     path('notes/', include('pim.notes.urls')),
     path('accounts/', include('allauth.urls')),
     path('accounts/', include('pim.accounts.urls')),
-    path('__debug__', include('debug_toolbar.urls')),
-    path('tinymce/', include('tinymce.urls')),
-    path('rosetta/', include('rosetta.urls')),
     prefix_default_language=False,
 )
--- pim/main/views.py
+++ pim/main/views.py
@@ -1,7 +1,30 @@
+from django.conf import settings
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.utils import translation
 from django.views.generic import ListView
 
-from pim.billing.models.plans import Plan
+from ..billing.models import Plan
+
+
+def set_language(request):
+    redirect_path = '/'
+    response = HttpResponseRedirect(redirect_path)
+    if request.method == 'POST':
+        language = request.POST.get('language')
+        if language:
+            if language != settings.LANGUAGE_CODE and [
+                lang for lang in settings.LANGUAGES if lang[0] == language
+            ]:
+                redirect_path = f'/{language}/'
+        else:
+            return response
+
+        translation.activate(language)
+        response = HttpResponseRedirect(redirect_path)
+        response.set_cookie(settings.LANGUAGE_COOKIE_NAME, language)
+
+    return response
 
 
 class HomeView(ListView):
