16e1b8b8e8636af1f717f765dd210355d68cfd96
Innocent Zenda <zendainnocent@gmail.com>
2023-09-10 00:40:14 +0300

    todos: rewrite for views, urls and templates

--- pim/todos/urls.py
+++ pim/todos/urls.py
@@ -2,18 +2,11 @@ from django.urls import path
 
 from . import views
 
-app_name = "todos"
+app_name = 'todos'
 
 urlpatterns = [
-    path("", views.index, name="todos-index"),
-]
-
-htmx_urlpatterns = [
-    path('create/', views.create_todo, name='create-todo'),
-    path("delete/<int:id>", views.delete_todo, name="delete-todo"),
-    path("toggle/<int:id>", views.toggle_todo, name="toggle-todo"),
-    path("update/<int:id>", views.update_todo, name="update-todo"),
+    path('', views.index, name='todos-index'),
+    path('delete/<int:id>', views.delete_todo, name='delete-todo'),
+    path('toggle/<int:id>', views.toggle_todo, name='toggle-todo'),
     path('sort/', views.sort_todos, name='sort-todos'),
 ]
-
-urlpatterns += htmx_urlpatterns
--- pim/todos/views.py
+++ pim/todos/views.py
@@ -1,124 +1,92 @@
-from typing import Any
-
-from django.contrib import messages
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, QueryDict
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.http import HttpRequest
+from django.template.response import TemplateResponse
 
-from . import forms
-from .models import Todo, UserTodo
-from .utils import reorder
-
-
-class TodoCreateView(CreateView):
-    model = Todo
-    form_class = forms.TodoForm
-    success_url = reverse_lazy('todos:todos-index')
-
-    def get_initial(self) -> dict[str, Any]:
-        return {'user': self.request.user}
-
-
-create_todo = TodoCreateView.as_view()
+from .models import Todo
 
 
 @login_required
-def index(request) -> HttpResponse:
-    template_name = 'todos/index.html'
-    user_todos = UserTodo.objects.prefetched_user_todo().filter(user=request.user)
-
-    context = {
-        'user_todos': user_todos,
-        'form': forms.TodoForm(),
-    }
-    return render(request, template_name, context)
-
-
-@login_required
-def delete_todo(request, id) -> HttpResponse:
-    todo = Todo.objects.prefetched_user().filter(user=request.user).get(id=id)
-
-    if todo.user == request.user:
-        user_todo = (
-            UserTodo.objects.prefetched_user_todo()
-            .filter(user=request.user)
-            .get(todo=todo)
-        )
-        user_todo.delete()
-        reorder(request.user)
-        messages.success(request, 'Todo deleted successful.')
-
-    user_todos = UserTodo.objects.prefetched_user_todo().filter(user=request.user)
-
-    context = {
-        'user_todos': user_todos,
-    }
-    return render(request, 'todos/partials/list.html', context)
+def index(request: HttpRequest) -> TemplateResponse:
+    return TemplateResponse(
+        request,
+        'todos/index.html',
+        {
+            'todos': request.user.todos.all(),
+        },
+    )
 
 
 @login_required
-def toggle_todo(request, id) -> HttpResponse | None:
-    template_name = 'todos/partials/checkbox.html'
+def delete_todo(request: HttpRequest, id: int) -> TemplateResponse:
+    request.user.todos.get(id=id).delete()
+    return TemplateResponse(
+        request,
+        'todos/partials/list.html',
+        {
+            'todos': request.user.todos.all(),
+        },
+    )
 
+
+@login_required
+def toggle_todo(request: HttpRequest, id: int) -> TemplateResponse | None:
     if request.method == 'POST':
-        todo = Todo.objects.prefetched_user().filter(user=request.user).get(id=id)
-
-        if todo.user == request.user:
-            todo.is_completed = not todo.is_completed
-            todo.save()
-
-        user_todo = UserTodo.objects.prefetched_user_todo().get(
-            user=request.user, todo=todo
+        todo: Todo = request.user.todos.get(id=id)
+        todo.is_completed = not todo.is_completed
+        todo.save()
+        return TemplateResponse(
+            request,
+            'todos/partials/checkbox.html',
+            {
+                'todo': todo,
+            },
         )
-        return render(request, template_name, {'todo': user_todo.todo})
 
 
 @login_required
-def update_todo(request, id) -> HttpResponse:
-    todo = Todo.objects.prefetched_user().get(id=id)
+def sort_todos(request: HttpRequest) -> TemplateResponse:
+    user: AbstractBaseUser = request.user
+    current_todos_order = request.POST.getlist('todo')
 
-    if request.method == 'PUT':
-        data = QueryDict(request.body)
-        if todo.user == request.user:
-            todo.text = data.get('todo')
-            todo.save()
-            messages.success(request, 'Todo updated successful.')
-            return HttpResponse(status=204)
-        else:
-            messages.error(request, 'You are not have access to update this todo.')
+    new_ordered_todos = []
 
-        user_todo = (
-            UserTodo.objects.prefetched_user_todo()
-            .filter(user=request.user)
-            .get(todo=todo)
-        )
-        return render(request, 'todos/partials/todo.html', {'user_todo': user_todo})
+    for index, todo_pk in enumerate(current_todos_order, start=1):
+        todo: Todo = user.todos.get(pk=todo_pk)
+        todo.order = index
+        new_ordered_todos.append(todo)
 
-    context = {'todo': todo}
-    return render(request, 'todos/partials/form.html', context)
+    user.todos.bulk_update(new_ordered_todos, fields=['order'])
+
+    return TemplateResponse(
+        request,
+        'todos/partials/list.html',
+        {
+            'todos': user.todos.all(),
+        },
+    )
 
 
-@login_required
-def sort_todos(request) -> HttpResponse:
-    current_todos_order = request.POST.getlist('user_todo')
+# @login_required
+# def update_todo(request, id) -> HttpResponse:
+#     todo = Todo.objects.prefetched_user().get(id=id)
 
-    new_ordered_user_todos = []
+#     if request.method == 'PUT':
+#         data = QueryDict(request.body)
+#         if todo.user == request.user:
+#             todo.text = data.get('todo')
+#             todo.save()
+#             messages.success(request, 'Todo updated successful.')
+#             return HttpResponse(status=204)
+#         else:
+#             messages.error(request, 'You are not have access to update this todo.')
 
-    for index, user_todo_pk in enumerate(current_todos_order, start=1):
-        user_todo = (
-            UserTodo.objects.prefetched_user_todo()
-            .filter(user=request.user)
-            .get(pk=user_todo_pk)
-        )
-        user_todo.order = index
-        new_ordered_user_todos.append(user_todo)
+#         user_todo = (
+#             UserTodo.objects.prefetched_user_todo()
+#             .filter(user=request.user)
+#             .get(todo=todo)
+#         )
+#         return render(request, 'todos/partials/todo.html', {'user_todo': user_todo})
 
-    UserTodo.objects.bulk_update(new_ordered_user_todos, fields=['order'])
-
-    user_todos = UserTodo.objects.prefetched_user_todo().filter(user=request.user)
-
-    template_name = 'todos/partials/list.html'
-    context = {'user_todos': user_todos}
-    return render(request, template_name, context)
+#     context = {'todo': todo}
+#     return render(request, 'todos/partials/form.html', context)
